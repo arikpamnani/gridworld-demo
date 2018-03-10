7c22cd5a2af0831b16fae7e56a09da97021c2433
Arik Pamnani <arikpamnani@gmail.com>
2018-03-10 23:09:47 +0530

    added policy evaluation

--- maingrid.js
+++ maingrid.js
@@ -25,6 +25,7 @@ GridWorld1.prototype = {
 					width: this.width,
 					height: this.height,
 					qval: 0.0,
+					isTarget: ((r==0&&c==0)||(r==this.nrows-1&&c==this.ncols-1)?1:0),
 					color: ((r==0&&c==0)||(r==this.nrows-1&&c==this.ncols-1)?"#00FF7F":"#fff")
 				};
 				row.push(column);
@@ -34,14 +35,67 @@ GridWorld1.prototype = {
 
 	},
 	
-	// updates this.gridData
+	// updates this.gridData using DP
 	updateData: function(){
+		// temp grid
+		var tempGrid=new Array();
+		for(var r=0; r<this.nrows; r++){
+			tempGrid.push(new Array(this.nols));
+		}
+
+		this.gamma=1;
+		this.reward=-1;
+
+		var actions=[-1, 1];
+
+		// Policy Evaluation using DP
 		for(var r=0; r<this.nrows; r++){
 			for(var c=0; c<this.ncols; c++){
+				if(this.gridData[r][c].isTarget){
+					tempGrid[r][c]=0.0;
+					continue;
+				}
 
-				// update q-table using DP
-				this.gridData[r][c].qval=+Math.random().toFixed(2);
+				var v=0;
 				
+				// left-right
+				for(let i of actions){
+					// agent remains in the same state
+					if(r+i<0 || r+i>=this.nrows)
+						v=v + this.reward + this.gamma*this.gridData[r][c].qval;
+
+					// agent reaches the target
+					else if(this.gridData[r+i][c].isTarget)
+						v=v + this.reward;
+
+					else{
+						v=v + this.reward + this.gamma*this.gridData[r+i][c].qval;
+					}
+				}
+
+				// up-down
+				for(let j of actions){
+					// agent remains in the same state
+					if(c+j<0 || c+j>=this.ncols)
+						v=v + this.reward + this.gamma*this.gridData[r][c].qval;
+					
+					// agent reaches the target
+					else if(this.gridData[r][c+j].isTarget)
+						v=v + this.reward;
+
+					else{
+						v=v + this.reward + this.gamma*this.gridData[r][c+j].qval;
+					}
+				}
+
+				tempGrid[r][c]=Number((v/4).toFixed(2));
+			}
+		}
+
+		// update this.gridData
+		for(var r=0; r<this.nrows; r++){
+			for(var c=0; c<this.ncols; c++){
+				this.gridData[r][c].qval=tempGrid[r][c];
 			}
 		}
 	},
@@ -93,6 +147,7 @@ GridWorld1.prototype = {
 				.selectAll(".txt")
 				.text(function(d) {return d.qval} );
 	}
+
 }
 
 var GridWorld2 = function(){
@@ -116,9 +171,9 @@ GridWorld2.prototype = {
 					width: this.width,
 					height: this.height,
 					color: ((r==0&&c==0)||(r==this.nrows-1&&c==this.ncols-1)?"#00FF7F":"#fff"),
-
+		
 					// left, right, up, down
-					dir: {l: 0.01, r: 0.01, u: 0.01, d: 0.01}
+					dir: (gridWorld1.gridData[r][c].isTarget?{l:0.01,r:0.01,u:0.01,d:0.01}:{l: 0.9, r: 0.9, u: 0.9, d: 0.9}),
 				};
 				row.push(column);
 			}
@@ -127,18 +182,53 @@ GridWorld2.prototype = {
 
 	},
 	
-	// updates this.gridData
+	// updates this.gridData, arrows are shown for directions having the maximum
+
 	updateData: function(){
-		// maximum reward
+		var actions=[-1, 1];
 
 		for(var r=0; r<this.nrows; r++){
 			for(var c=0; c<this.ncols; c++){
+				if(gridWorld1.gridData[r][c].isTarget)
+					continue;
 
-				// random update (for now)
-				this.gridData[r][c].dir.l=Math.random();
-				this.gridData[r][c].dir.r=Math.random();
-				this.gridData[r][c].dir.u=Math.random();
-				this.gridData[r][c].dir.d=Math.random();
+				// reinitialize maximum
+				var maxval=-1e9;
+
+				// fetching the maximum
+				for(let i of actions){
+					if(r+i>=0 && r+i<this.nrows)
+						maxval=Math.max(maxval, gridWorld1.gridData[r+i][c].qval);
+				}
+
+				for(let j of actions){
+					if(c+j>=0 && c+j<this.ncols)
+						maxval=Math.max(maxval, gridWorld1.gridData[r][c+j].qval);
+				}
+				
+				// up arrow length 
+				if(r-1>=0 && gridWorld1.gridData[r-1][c].qval==maxval)
+					this.gridData[c][r].dir.u=0.9;
+				else
+					this.gridData[c][r].dir.u=0.01;
+				
+				// down arrow length 
+				if(r+1>=0 && r+1<this.nrows && gridWorld1.gridData[r+1][c].qval==maxval)
+					this.gridData[c][r].dir.d=0.9;
+				else
+					this.gridData[c][r].dir.d=0.01;
+
+				// left arrow length 
+				if(c-1>=0 && c-1<this.ncols && gridWorld1.gridData[r][c-1].qval==maxval)
+					this.gridData[c][r].dir.l=0.9;
+				else
+					this.gridData[c][r].dir.l=0.01;
+
+				// right arrow length 
+				if(c+1>=0 && c+1<this.ncols && gridWorld1.gridData[r][c+1].qval==maxval)
+					this.gridData[c][r].dir.r=0.9;
+				else
+					this.gridData[c][r].dir.r=0.01;
 			}
 		}
 	},
@@ -259,11 +349,11 @@ GridWorld2.prototype = {
 
 
 var gridWorld1 = new GridWorld1();
-gridWorld1.initData(7, 7);
+gridWorld1.initData(4, 4);
 gridWorld1.initGrid();
 
 var gridWorld2 = new GridWorld2();
-gridWorld2.initData(7, 7);
+gridWorld2.initData(4, 4);
 gridWorld2.initGrid();
 
 
@@ -278,13 +368,25 @@ updateButton.onclick = function(){
 	gridWorld2.updateGrid();
 }
 
-var iterateButton=document.querySelector("button.iterate");
-iterateButton.onclick = function(){
-	/*d3.interval(function(){
+var play=function(){
+	var time=0;
+	var timer=setInterval(function(){
+		time++;
+		
+		// updates
 		gridWorld1.updateData();
 		gridWorld1.updateGrid();
 
 		gridWorld2.updateData();
 		gridWorld2.updateGrid();
-	}, 100);*/
+
+		if(time>100)
+			clearInterval(timer);
+
+	}, 100);
+}
+
+var iterateButton=document.querySelector("button.iterate");
+iterateButton.onclick = function(){
+	play();
 }
